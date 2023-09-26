827c833bd51e5af76a28f6c2722f27b59e2848f7
Unitato0315 <jvsonic9@gmail.com>
2023-09-26 19:43:54 +0200

    Primera Version

---
+++ src/main/java/es/josevicente/migit2/Migit2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package es.josevicente.migit2;
+
+/**
+ *
+ * @author Jose Vicente Vargas Mestanza <jvsonic9@gmail.com>
+ */
+public class Migit2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
