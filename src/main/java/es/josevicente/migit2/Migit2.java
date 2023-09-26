9762e5ec7831500396c44b24be13652d804422dd
Unitato0315 <jvsonic9@gmail.com>
2023-09-26 19:53:22 +0200

    Primera Version

--- src/main/java/es/josevicente/migit2/Migit2.java
+++ src/main/java/es/josevicente/migit2/Migit2.java
@@ -13,5 +13,6 @@ public class Migit2 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("probemos cosas por la cara");
     }
 }
